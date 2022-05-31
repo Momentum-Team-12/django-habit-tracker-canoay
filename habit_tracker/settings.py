18d69eb86600ecc17ab01d0258a929c67a284284
Ambar Cano <ambarcano@gmail.com>
2022-05-31 13:59:43 -0400

    API works ok, one more thing

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -147,3 +147,9 @@ AUTH_USER_MODEL = "habit.CustomUser"
 # heroku settings
 django_on_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
